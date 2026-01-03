dff56fbd50315de7fff3d36029714ec36a1111bc
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-01-03 23:31:52 +0700

    chore: setup static export + gh-pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/my-recap-ejj" : "",
+  assetPrefix: isProd ? "/my-recap-ejj/" : "",
+};
 
 export default nextConfig;
+
