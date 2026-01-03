eb4619ede6738f75b72c57cff5aa000a8a202808
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-01-04 00:50:14 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,17 @@
 
 // export default nextConfig;
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const repo = "my-recap-ejj";
 
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/my-recap-ejj" : "",
-  assetPrefix: isProd ? "/my-recap-ejj/" : "",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
+
 
