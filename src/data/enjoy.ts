2a9f7b73869b2db1ef11c5653ec4cd149638e74f
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-01-11 20:53:04 +0700

    type-data-attachment

---
+++ src/data/enjoy.ts
@@ -0,0 +1,52 @@
+import { Profile } from "@/types/profile";
+
+export const enjoyData: Profile = {
+    firstnameTH: "ธิดารัตน์",
+    firstnameEN: "Thidarat",
+    lastnameTH: "ปรือทอง",
+    lastnameEN: "Pruethong",
+    nicknameTH: "เอนจอย",
+    nicknameEN: "Enjoy",
+    birthday: "1997-10-13",
+    hashtag: ["#Enjoyyotdr", "#เอนจอยธิดารัตน์"],
+    LikesAndDislikes: {
+        likeColor: ["ขาว","ดำ"],
+        likeFood: ["ส้มตำ", "ปิ้งย่าง", "ชาบู"],
+        likePet: ["สุนัข"],
+        likeHobby: ["ออกกำลังกาย"],
+        likeSport: ["เทนนิส", "แบดมินตัน", "พิคเคิลบอล"],
+        likeCollectible: ["น้ำหอม"],
+        likeArtist: [],
+        likeMusic: [],
+        likeMovie: [],
+        likePlace: ["ทะเล" , "ภูเขา", "ธรรมชาติ", "อควาเรียม"],
+    },
+    SocialLink: {
+        instagram: "https://www.instagram.com/enjoyyotdr",
+        thread: "https://www.threads.com/@enjoyyotdr",
+        tiktok: "https://www.tiktok.com/@enjoyyotdr",
+        twitter: "https://x.com/enjoyyotdrr",
+        facebook: "https://www.facebook.com/enjoyy.thidarat"
+    },
+    ProfileImage: [
+        {
+            name: "เอนจอย",
+            image: "/enjoy/enjoy__1.jpg",
+        },
+        {
+            name: "พี่ธิ",
+            image: "/enjoy/enjoy__3.jpg",
+        },
+        {
+            name: "เอนจ๋อง",
+            image: "/enjoy/enjoy__2.jpg",
+        }
+    ],
+    ContactForWork: {
+        name: "googolfgolf ",
+        entertainment: ["ConversationThailand","CopyaBangkok"],
+        instagram: "@googolfgolf",
+        mobile: "0818437109",
+        line: "@workwithenjoy"
+    }
+}
---
+++ src/types/profile.ts
@@ -0,0 +1,80 @@
+export type Profile = {
+    firstnameTH: string,
+    firstnameEN: string,
+    lastnameTH: string,
+    lastnameEN: string,
+    nicknameTH: string,
+    nicknameEN: string,
+    birthday: string,
+    // aka?: string[],
+    hashtag?: string[],
+    LikesAndDislikes?: LikesAndDislikes,
+    Allergy?: string[],
+    SocialLink?: SocialLink,
+    Brands?: Brand[],
+    // Images?: string,
+    ProfileImage?: ProfileImage[],
+    Besties?: Bestie[],
+    ContactForWork?: ContactForWork,
+    OfficialFanclub?: OfficialFanclub
+}
+export type ProfileImage = {
+    name?: string,
+    image?: string
+}
+export type SocialLink = {
+    facebook?: string,
+    instagram?: string,
+    twitter?: string,
+    tiktok?: string,
+    thread?: string
+}
+export type Brand = {
+    name: string,
+    facebook?: string,
+    instagram?: string,
+    twitter?: string,
+    tiktok?: string,
+}
+export type Bestie = {
+    index: number
+    relationship? : string
+    name?: string
+    facebook?: string,
+    instagram?: string,
+    twitter?: string,
+    tiktok?: string,
+    Image?: string
+}
+export type ContactForWork = {
+    name?: string,
+    entertainment?: string[],
+    facebook?: string,
+    instagram?: string,
+    twitter?: string,
+    tiktok?: string,
+    mobile?: string,
+    email?: string,
+    line?: string
+}
+export type LikesAndDislikes = {
+    likeColor? : string[],
+    likeFood? : string[],
+    dislikeFood? : string[],
+    likePet? : string[],
+    likeHobby? : string[],
+    likeSport? : string[],
+    likeCollectible? : string[],
+    likeArtist? : string[],
+    likeMusic? : string[],
+    likeMovie? : string[],
+    likePlace? : string[],
+}
+export type OfficialFanclub = {
+    name?: string,
+    facebook?: string,
+    instagram?: string,
+    thread?: string,
+    twitter?: string,
+    tiktok?: string
+}
