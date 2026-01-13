2808ffd9e94a826214dde52773d04c159566b623
ppaanjkr <88655177+ppaanjkr@users.noreply.github.com>
2026-01-13 17:56:57 +0700

    [deloy] set cname when deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const repo = "my-recap-ejj"; 
-
-// const nextConfig = {
-//   output: "export",
-//   images: { unoptimized: true },
-//   // แก้ไข: นำ / ตัวสุดท้ายออกที่ assetPrefix
-//   basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-//   assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-// };
 
 // export default nextConfig;
 /** @type {import('next').NextConfig} */
@@ -16,8 +6,7 @@ const repo = "my-recap-ejj";
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  // assetPrefix: `/${repo}`,
+  basePath: "",
 };
 
 export default nextConfig;
